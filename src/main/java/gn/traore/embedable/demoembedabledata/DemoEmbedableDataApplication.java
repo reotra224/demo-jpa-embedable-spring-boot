c37c4f3d93a6c661d04d2a4d02fa92a831987341
Ibrahima <ibrahima982011@koonda.fr>
2020-02-12 00:24:57 +0000

    Initialisation du Projet.

---
+++ src/main/java/gn/traore/embedable/demoembedabledata/DemoEmbedableDataApplication.java
@@ -0,0 +1,13 @@
+package gn.traore.embedable.demoembedabledata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEmbedableDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEmbedableDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gn/traore/embedable/demoembedabledata/DemoEmbedableDataApplicationTests.java
@@ -0,0 +1,13 @@
+package gn.traore.embedable.demoembedabledata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEmbedableDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
